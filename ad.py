72e9417517c16efecd3e98cee4049e931be3bb49
ArianaDeAbreu <55302851+ArianaDeabreu@users.noreply.github.com>
2023-03-03 09:10:40 -0300

    V.1

---
+++ ad.py
@@ -0,0 +1,491 @@
+a = [
+      ['nome', 'Alan', 'Amanda', 'Alisson', 'Aline', 'alvira'],
+      ['cep', ''],
+      ['animal', 'andorinha', 'abutre', 'anaconda', 'atum', 'anchova', 'arenque', 'albatroz', 'alpaca'],
+      ['fruta', 'açai', 'amora', 'acerola', 'abacaxi','abacate'],
+      ['alimento', 'almôndegas','Arroz','acarajé'],
+      ['cor', 'azul','abóbora', 'âmbar', 'Amarelo'],
+      ['objeto', 'anel', 'agulha', 'abafador', 'algema', 'agenda', 'adaga'],
+      ['profisão', 'alfaiate', 'artesão', 'arquiteto','arqueólogo','açougueiro'],
+      ['adjetivo', 'amável', 'agradecida','afetuosa', 'acolhedora', 'ágil' ],
+      ['personagem', 'ajax (vandinha)', 'aquaman', 'akira (death note)', 'apu (simpsons)', 'arlong(one piece)'],
+      ['marca', 'adidas', 'allora', 'acer', 'americanas'],
+      ['filme', 'a caçada', 'a casa de cera', 'a bela e a fera'],
+      ['cantor', 'alicia keys', 'adele', 'ariana grande', 'anitta', 'avril lavigne', 'alanis morissete'],
+      ['ator','adam sandler', 'ary toledo', 'alexis texas'],
+      ['música', 'all i want (mariah carey)', 'a sky full of stars (coldplay)'],
+      ]
+b = [
+      ['nome', 'brunesa', 'bernadete', 'braulio', 'bart', 'boris', 'bjorn'],
+      ['cep', 'budapeste', 'berlim', 'belgica', 'belize', 'bahamas'],
+      ['animal', 'bem-te-vi', 'beija-flor', 'bacalhau', 'bagre', 'baiacu', 'boto', 'bicho-da-seda'],
+      ['fruta', 'banana', 'bergamota'],
+      ['alimento', 'bolo', 'bolinho de chuva', 'brigadeiro', 'bife e fritas'],
+      ['cor'],
+      ['objeto', ],
+      ['profisão', 'bailarino', 'biólogo', 'bancario', 'baterista', 'bodybuilder', 'biomedico'],
+      ['adjetivo', 'batalhadora','benevolente', 'brincalhona'],
+      ['personagem', 'barney (how i met your mother)', 'bart simpson', 'bidu (turma da mônica)', 'bobo esponja', ''],
+      ['marca', 'big', 'barbie', 'burguer king', 'bayer', 'budwiser'],
+      ['filme', 'burlesque', 'batman'],
+      ['cantor', 'bruno mars', 'beyoncé', 'bts','billie eilish', 'black eyed peas'],
+      ['atriz', 'bruna marquezine', 'beth carvalho', 'brad pitt'],
+      ['música', 'break free (ariana)', 'bad romance (lady gaga)', 'billie jean (michael jackson)', 'beautiful (christina aguilera )'],
+      ]
+c = [
+      ['nome','cateryne', 'carine', 'carolyne', 'cassiane'],
+      ['cep', ],
+      ['animal', 'castor', 'cutia', 'capivara', 'cascavel', 'cisne', 'cegonha', 'cavalo-marinho', 'chacal', 'condor', 'coiote', 'codorna'],
+      ['fruta', 'cupuaçu', 'cereja', 'carambola', 'caju', 'cacau'],
+      ['alimento', 'cuzcuz', 'canjica', 'cachorro-quente'],
+      ['cor', 'castanho', 'carmim', 'caramelo', 'creme', 'ciano', 'cinza'],
+      ['objeto', 'balde', 'bazuca', 'bisturi', 'brinco', 'bule'],
+      ['profisão', 'cosmologo', 'cantor', 'compositor', 'capitão', 'contador'],
+      ['adjetivo', 'criativa', 'carinhosa', 'calma', 'carente'],
+      ['personagem', 'castiel', 'cebolinha', 'choji (naruto)', 'coringa'],
+      ['marca', 'c&a', 'casio', 'canon', 'carrefour', 'coca cola'],
+      ['filme', 'constantine', 'criminal minds', 'creed', 'caça às bruxas'],
+      ['cantor', 'coldplay', 'celine dion', 'christina aguilera', 'christina perri', 'christina grimmie', 'cardi b'],
+      ['atriz', 'caio castro', 'cláudia raia'],
+      ['música', 'chasing cars (snow patrol)', 'clocks (coldplay)', 'chop suey (sistem of down)', 'complicated (avril lavigne)'],
+      ]
+d = [
+      ['nome', 'denise', 'diego', 'dana', 'daniele', 'damião', 'daliataria'],
+      ['cep', 'dinamarca', 'dallas'],
+      ['animal', 'dromedálho', 'doninha', 'dinossauro', 'dragão', 'dragão-de-komodo', 'diabo-da-tasmânia'],
+      ['fruta', 'damasco'],
+      [alimento, ],
+      [cor ,],
+      [objeto, ],
+      [profisão, ],
+      [adjetivo, ],
+      [personagem, ],
+      [marca, ],
+      [filme, ],
+      [cantor, ],
+      [atriz, ],
+      [música, ],
+      [desenho, ]
+      ]
+e = [
+      [nome,],
+      [cep, ],
+      ['animal', 'enguia', 'esquilo', 'esturjão', 'enchova', 'escorpião', 'estrela-do-mar', 'escaravelho'],
+      ['fruta', '???'],
+      [alimento, ],
+      [cor ,],
+      [objeto, ],
+      [profisão, ],
+      [adjetivo, ],
+      [personagem, ],
+      [marca, ],
+      [filme, ],
+      [cantor, ],
+      [atriz, ],
+      [música, ],
+      [desenho, ]
+      ]
+f = [
+      [nome,],
+      [cep, ],
+      ['animal', 'furão', 'faisão', 'flamingo', 'fuinha'],
+      ['fruta', 'Figo', 'Framboesa'],
+      [alimento, ],
+      [cor ,],
+      [objeto, ],
+      [profisão, ],
+      [adjetivo, ],
+      [personagem, ],
+      [marca, ],
+      [filme, ],
+      [cantor, ],
+      [atriz, ],
+      [música, ],
+      [desenho, ]
+      ]
+g = [
+      [nome,],
+      [cep, ],
+      ['animal', 'galo', 'ganso', 'golfinho', 'gralha', 'gazela', 'grilo', 'gnu', 'guaxinim', 'gambá'],
+      ['fruta', 'goiaba','Graviola'],
+      [alimento, ],
+      [cor ,],
+      [objeto, ],
+      [profisão, ],
+      [adjetivo, ],
+      [personagem, ],
+      [marca, ],
+      [filme, ],
+      [cantor, ],
+      [atriz, ],
+      [música, ],
+      [desenho, ]
+      ]
+h = [
+      [nome,],
+      [cep, ],
+      ['animal', 'harpia', 'hiena', 'hamster'],
+      ['fruta', '???'],
+      [alimento, ],
+      [cor ,],
+      [objeto, ],
+      [profisão, ],
+      [adjetivo, ],
+      [personagem, ],
+      [marca, ],
+      [filme, ],
+      [cantor, ],
+      [atriz, ],
+      [música, ],
+      [desenho, ]
+      ]
+i = [
+      [nome,],
+      [cep, ],
+      ['animal', 'iguana', 'impala'],
+      ['fruta', '???'],
+      [alimento, ],
+      [cor ,],
+      [objeto, ],
+      [profisão, ],
+      [adjetivo, ],
+      [personagem, ],
+      [marca, ],
+      [filme, ],
+      [cantor, ],
+      [atriz, ],
+      [música, ],
+      [desenho, ]
+      ]
+j = [
+      [nome,],
+      [cep, ],
+      ['animal', 'jiboia', 'jaguar', 'jabuti', 'jegue', 'joão-de-barro', 'jararaca', 'joaninha'],
+      ['fruta', 'jaca', 'jambo', 'jamelão', 'jabuticaba'],
+      [alimento, ],
+      [cor ,],
+      [objeto, ],
+      [profisão, ],
+      [adjetivo, ],
+      [personagem, ],
+      [marca, ],
+      [filme, ],
+      [cantor, ],
+      [atriz, ],
+      [música, ],
+      [desenho, ]
+      ]
+k = [
+      [nome,],
+      [cep, ],
+      ['animal', '???'],
+      ['fruta', 'kiwi'],
+      [alimento, ],
+      [cor ,],
+      [objeto, ],
+      [profisão, ],
+      [adjetivo, ],
+      [personagem, ],
+      [marca, ],
+      [filme, ],
+      [cantor, ],
+      [atriz, ],
+      [música, ],
+      [desenho, ]
+      ]
+l = [
+      [nome,],
+      [cep, ],
+      ['animal', 'leopardo', 'lagosta', 'lula', 'lobo', 'lontra', 'lhama', 'lince','lesma','libélula','lacraia', 'linguado'],
+      ['fruta', 'laranja', 'limão'],
+      [alimento, ],
+      [cor ,],
+      [objeto, ],
+      [profisão, ],
+      [adjetivo, ],
+      [personagem, ],
+      [marca, ],
+      [filme, ],
+      [cantor, ],
+      [atriz, ],
+      [música, ],
+      [desenho, ]
+      ]
+m = [
+      [nome,],
+      [cep, ],
+      ['animal', 'mexilhão','mamute','morsa','marmota','moreia','mariposa','marimbondo'],
+      ['fruta', 'mamão', 'maça', 'morango', 'maracujá' ],
+      [alimento, ],
+      [cor ,],
+      [objeto, ],
+      [profisão, ],
+      [adjetivo, ],
+      [personagem, ],
+      [marca, ],
+      [filme, ],
+      [cantor, ],
+      [atriz, ],
+      [música, ],
+      [desenho, ]
+      ]
+n = [
+      [nome,],
+      [cep, ],
+      ['animal', 'naja'],
+      ['fruta', '???'],
+      [alimento, ],
+      [cor ,],
+      [objeto, ],
+      [profisão, ],
+      [adjetivo, ],
+      [personagem, ],
+      [marca, ],
+      [filme, ],
+      [cantor, ],
+      [atriz, ],
+      [música, ],
+      [desenho, ]
+      ]
+o = [
+      [nome,],
+      [cep, ],
+      ['animal', 'ouriço', 'ornintorrinco', 'orangotango', 'orca'],
+      ['fruta', 'oiti'],
+      [alimento, ],
+      [cor ,],
+      [objeto, ],
+      [profisão, ],
+      [adjetivo, ],
+      [personagem, ],
+      [marca, ],
+      [filme, ],
+      [cantor, ],
+      [atriz, ],
+      [música, ],
+      [desenho, ]
+      ]
+p = [
+      [nome,],
+      [cep, ],
+      ['animal', 'pavão', 'pirarucu', 'polvo', 'pelicano', 'peru', 'preá', 'pônei', 'pantera', 'pulga'],
+      ['fruta', 'pitaia', 'pitanga', 'pêssego'],
+      [alimento, ],
+      [cor ,],
+      [objeto, ],
+      [profisão, ],
+      [adjetivo, ],
+      [personagem, ],
+      [marca, ],
+      [filme, ],
+      [cantor, ],
+      [atriz, ],
+      [música, ],
+      [desenho, ]
+      ]
+q = [
+      [nome,],
+      [cep, ],
+      ['animal', 'quati', 'quero-quero', 'quimera'],
+      ['fruta', '???',],
+      [alimento, ],
+      [cor ,],
+      [objeto, ],
+      [profisão, ],
+      [adjetivo, ],
+      [personagem, ],
+      [marca, ],
+      [filme, ],
+      [cantor, ],
+      [atriz, ],
+      [música, ],
+      [desenho, ]
+      ]
+r = [
+      [nome,],
+      [cep, ],
+      [animal, 'ratazana', 'rolina','raia','rouxinol', 'rã', 'rola'],
+      ['fruta', 'romã'],
+      [alimento, ],
+      [cor ,],
+      [objeto, ],
+      [profisão, ],
+      [adjetivo, ],
+      [personagem, ],
+      [marca, ],
+      [filme, ],
+      [cantor, ],
+      [atriz, ],
+      [música, ],
+      [desenho, ]
+      ]
+s = [
+      [nome,],
+      [cep, ],
+      ['animal', 'sabiá', 'siri', 'salmão', 'sagui', 'sucuri', 'salamandra', 'suricate'],
+      ['fruta','???'],
+      [alimento, ],
+      [cor ,],
+      [objeto, ],
+      [profisão, ],
+      [adjetivo, ],
+      [personagem, ],
+      [marca, ],
+      [filme, ],
+      [cantor, ],
+      [atriz, ],
+      [música, ],
+      [desenho, ]
+      ]
+t = [
+      [nome,],
+      [cep, ],
+      ['animal', 'tainha','tritão', 'tubarão','tucano', 'truta', 'tatu'],
+      ['fruta', 'tangirina'],
+      [alimento, ],
+      [cor ,],
+      [objeto, ],
+      [profisão, ],
+      [adjetivo, ],
+      [personagem, ],
+      [marca, ],
+      [filme, ],
+      [cantor, ],
+      [atriz, ],
+      [música, ],
+      [desenho, ]
+      ]
+u = [
+      [nome,],
+      [cep, ],
+      ['animal', 'urubu', 'urso panda'],
+      ['fruta', 'uva'],
+      [alimento, ],
+      [cor ,],
+      [objeto, ],
+      [profisão, ],
+      [adjetivo, ],
+      [personagem, ],
+      [marca, ],
+      [filme, ],
+      [cantor, ],
+      [atriz, ],
+      [música, ],
+      [desenho, ]
+      ]
+v = [
+      [nome,],
+      [cep, ],
+      ['animal', 'vespa', 'vagalume', 'lemure', 'viúva negra', 'víbora'],
+      ['fruta','???' ],
+      [alimento, ],
+      [cor ,],
+      [objeto, ],
+      [profisão, ],
+      [adjetivo, ],
+      [personagem, ],
+      [marca, ],
+      [filme, ],
+      [cantor, ],
+      [atriz, ],
+      [música, ],
+      [desenho, ]
+      ]
+x = [
+      [nome,],
+      [cep, ],
+      [animal, ],
+      ['fruta','???' ],
+      [alimento, ],
+      [cor ,],
+      [objeto, ],
+      [profisão, ],
+      [adjetivo, ],
+      [personagem, ],
+      [marca, ],
+      [filme, ],
+      [cantor, ],
+      [atriz, ],
+      [música, ],
+      [desenho, ]
+      ]
+
+w = [
+      [nome,],
+      [cep, ],
+      ['animal', '???'],
+      ['fruta','???'],
+      [alimento, ],
+      [cor ,],
+      [objeto, ],
+      [profisão, ],
+      [adjetivo, ],
+      [personagem, ],
+      [marca, ],
+      [filme, ],
+      [cantor, ],
+      [atriz, ],
+      [música, ],
+      [desenho, ]
+      ]
+
+y = [
+      [nome,],
+      [cep, ],
+      ['animal', '???'],
+      ['fruta', '???'],
+      [alimento, ],
+      [cor ,],
+      [objeto, ],
+      [profisão, ],
+      [adjetivo, ],
+      [personagem, ],
+      [marca, ],
+      [filme, ],
+      [cantor, ],
+      [atriz, ],
+      [música, ],
+      [desenho, ]
+      ]
+z = [
+      ['nome','Zendaia','Zumira','zany', 'zayan'],
+      [cep, ],
+      ['animal', 'zebra','zebu', 'zangão'],
+      ['fruta', '???'],
+      [alimento, ],
+      [cor ,],
+      [objeto, ],
+      [profisão, ],
+      [adjetivo, ],
+      [personagem, ],
+      [marca, ],
+      [filme, ],
+      [cantor, ],
+      [atriz, ],
+      [música, ],
+      [desenho, ]
+      ]
+
+
+
+def adedonha(categorias, letra):
+
+
+     return None
+
+categorias = str(input('1- nome\n2- cep\n3- comida\n4- marca\n5- profissão\n6- musica\n7- cantor\n8- ator\n9- perosnagem\n10 -cor\n11- objeto\n12- filme\n\nDigite o número de cada categoria separado por vírgula: '))
+
+letra = str(input('Letra: ')).split(',')
+
+print(adedonha(categorias, letra))
+
+
+'''
+confirmarCategoria = str(input('Apenas essas categorias? S/N \n'))
+while confirmarCategoria = str('n') or str('N'):
+     categorias = str(input('Digite o número de cada categoria separado
+por vírgula: '))
+else:
+     break
+
+'''
